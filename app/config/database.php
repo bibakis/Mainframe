550b15d3518320dd3f40ffe2419a2969371a16bb
Vangelis Bibakis <bibakisv@gmail.com>
2011-11-23 20:29:01 +0200

    Changed default database configuration. It now includes local & remote groups

--- app/config/database.php
+++ app/config/database.php
@@ -44,24 +44,40 @@
 | the active record class
 */
 
-$active_group = 'default';
+$active_group = 'local';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
-$db['default']['dbdriver'] = 'mysql';
-$db['default']['dbprefix'] = '';
-$db['default']['pconnect'] = TRUE;
-$db['default']['db_debug'] = TRUE;
-$db['default']['cache_on'] = FALSE;
-$db['default']['cachedir'] = '';
-$db['default']['char_set'] = 'utf8';
-$db['default']['dbcollat'] = 'utf8_general_ci';
-$db['default']['swap_pre'] = '';
-$db['default']['autoinit'] = TRUE;
-$db['default']['stricton'] = FALSE;
+$db['local']['hostname'] = 'localhost';
+$db['local']['username'] = '';
+$db['local']['password'] = '';
+$db['local']['database'] = '';
+$db['local']['dbdriver'] = 'mysql';
+$db['local']['dbprefix'] = '';
+$db['local']['pconnect'] = TRUE;
+$db['local']['db_debug'] = TRUE;
+$db['local']['cache_on'] = FALSE;
+$db['local']['cachedir'] = '';
+$db['local']['char_set'] = 'utf8';
+$db['local']['dbcollat'] = 'utf8_general_ci';
+$db['local']['swap_pre'] = '';
+$db['local']['autoinit'] = TRUE;
+$db['local']['stricton'] = FALSE;
+
+$db['remote']['hostname'] = 'localhost';
+$db['remote']['username'] = '';
+$db['remote']['password'] = '';
+$db['remote']['database'] = '';
+$db['remote']['dbdriver'] = 'mysql';
+$db['remote']['dbprefix'] = '';
+$db['remote']['pconnect'] = TRUE;
+$db['remote']['db_debug'] = TRUE;
+$db['remote']['cache_on'] = FALSE;
+$db['remote']['cachedir'] = '';
+$db['remote']['char_set'] = 'utf8';
+$db['remote']['dbcollat'] = 'utf8_general_ci';
+$db['remote']['swap_pre'] = '';
+$db['remote']['autoinit'] = TRUE;
+$db['remote']['stricton'] = FALSE;
 
 
 /* End of file database.php */
