74efa8a1c08ce3d8b354846ff9f46986b34c1bad
Kostas Loupasakis <loupax@gmail.com>
2012-01-11 15:01:46 +0200

    Created a few tests to ensure plugin functionality of helpers, libraries, helpers and models

--- app/plugins/demo_plugin/controllers/home.php
+++ app/plugins/demo_plugin/controllers/home.php
@@ -5,7 +5,20 @@ class Home extends CI_Controller
 	function index()
 	{
 		$this->load->theme('demo');
-		$this->load->view('plugin_home');
+		
+		$this->load->helper('demo');
+		$this->load->language('demo','english');
+		$this->load->library('demo_lib');
+		$this->load->model('demo_model');
+		
+		$data = array(
+			'helper'	=> test_helper_function(),
+			'language'	=> $this->lang->line('demo_message'),
+			'library'	=> $this->demo_lib->test_library(),
+			'model'		=> $this->demo_model->model_test(),
+		);
+		
+		$this->load->view('plugin_home',$data);
 	}
 	
 }
--- app/plugins/demo_plugin/views/demo/plugin_home.php
+++ app/plugins/demo_plugin/views/demo/plugin_home.php
@@ -15,7 +15,30 @@
 </p>
 
 <p>
+	<h4>Testing</h4>
+	<table class="styled">
+		<thead>
+			<tr>
+				<td>Plugin Helper</td>
+				<td>Plugin Library</td>
+				<td>Plugin Model</td>
+				<td>Plugin langugage</td>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td><?php echo $helper; ?></td>
+				<td><?php echo $library; ?></td>
+				<td><?php echo $model; ?></td>
+				<td><?php echo $language; ?></td>
+			</tr>
+		</tbody>
+	</table>
+</p>
+
+<p>
 	Check a plugin controller that is located inside of a 
 	<a href="<?php echo base_url().'demo_plugin/folder/test';?>">subfolder</a>
 	or download <a href="https://github.com/bibakis/Mainframe">Mainframe</a>
 </p>
+
