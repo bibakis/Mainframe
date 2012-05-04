d5b24d747734548120f988585fc40b33b90ac5af
Kostas Loupasakis <loupax@gmail.com>
2012-05-04 18:38:11 +0300

    Currently developing Dev Tools, currently Work In Progress
    Added Twitter Bootstrap to the default libraries

--- app/controllers/mainframe/assets.php
+++ app/controllers/mainframe/assets.php
@@ -1,38 +1,15 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-/*
- * Temporary controller, can be safely deleted at anytime
- */
 class Assets extends CI_Controller {
 
 	public function index()
 	{
 	}
 	
-	function css ()
-	{
-		$css_file = '/themes/demo/css/styles.css';
-		$css_code = file_get_contents($_SERVER['DOCUMENT_ROOT'] . $css_file);
-		
-		require_once '/libs/php/cssmin.php';
-		
-		$CSSmin = new CSSmin;
-		
-		$css_code = $CSSmin->run($css_code);
-		
-		echo $css_code;
-	}
 	
 	function js()
 	{
-		$js_file = '/libs/js/jquery.url.js';
-		$js_code = file_get_contents($_SERVER['DOCUMENT_ROOT'] . $js_file);
 		
-		require_once '/libs/php/JSMin.php';
-		
-		$js_code = JSMin::minify($js_code);
-		
-		echo $js_code;
 	}
 	
 }
