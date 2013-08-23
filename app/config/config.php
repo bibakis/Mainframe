16b27acd24b71223759820d41d5f07c1dd292d55
Vangelis Bibakis <bibakisv@gmail.com>
2013-08-23 18:29:42 +0300

    Changed default session duration to 2 years

--- app/config/config.php
+++ app/config/config.php
@@ -245,14 +245,14 @@ $config['encryption_key'] = 'change_this_before_deployment';
 |
 */
 $config['sess_cookie_name']		= 'ci_session';
-$config['sess_expiration']		= 7200;
+$config['sess_expiration']		= 0;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= FALSE;
 $config['sess_use_database']	= FALSE;
 $config['sess_table_name']		= 'ci_sessions';
 $config['sess_match_ip']		= FALSE;
 $config['sess_match_useragent']	= TRUE;
-$config['sess_time_to_update']	= 300;
+$config['sess_time_to_update']	= 60*60*24*365*2;
 
 /*
 |--------------------------------------------------------------------------
